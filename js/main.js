34cdb8c3da3b894f72b53b7368dbd227cb1291a9
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2019-06-12 18:32:50 +0300

    сделано автоформирование 8 маркеров на карте

--- js/main.js
+++ js/main.js
@@ -1,41 +1,36 @@
 'use strict';
 var pointsMap = [];
-var typeOffer = ['palace', 'flat', 'house', 'bungalo'];
-
+var type = ['palace', 'flat', 'house', 'bungalo'];
+var map = document.querySelector('.map');
 
 var randomPointMap = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
 for (var i = 0; i < 8; i++) {
-  	pointsMap[i] = {author: 'img/avatar/user0' + randomPointMap(1, 8) + '.png',
-    offter: typeOffer[randomPointMap(0, 3)],
-    locationX: randomPointMap(0, 130),
-	locationY: randomPointMap(130, 630)}
-		
-};
-
-
-var map = document.querySelector('.map');
-	map.classList.remove('map--faded');
-	
-var pointsTempl = document.querySelector('#pin').content.querySelector('.map__pin');	
-var pointsTemplImg = pointsTempl.querySelector('img');
-
-var pointView = function (pointTempl) {
-	var pointTemplClone = pointsTempl.cloneNode(true);
-	pointTemplClone.style.cssText = 'left: ' + pointTempl.locationX + 'px; top: ' + pointTempl.locationY;
-	pointsTemplImg.src = pointTempl.author;
-	pointsTemplImg.atl = pointTempl.author;
-	
-	return pointTemplClone;
+  pointsMap[i] = {
+    author: {avatar: './img/avatars/user0' + (i + 1) + '.png'},
+    offter: {type: type[randomPointMap(0, 3)]},
+    location: {x: randomPointMap(0, document.querySelector('main').offsetWidth),
+      y: randomPointMap(130, 630)}};
 }
 
+map.classList.remove('map--faded');
+
+var pointsTempl = document.querySelector('#pin').content.querySelector('.map__pin');
+
+var pointView = function (pointTempl) {
+  var pointTemplClone = pointsTempl.cloneNode(true);
+  pointTemplClone.style = 'left: ' + (pointTempl.location.x - document.querySelector('.map__pin').offsetWidth / 2) + 'px; top: ' + (pointTempl.location.y - document.querySelector('.map__pin').offsetHeight) + 'px;';
+  pointTemplClone.querySelector('img').src = pointTempl.author.avatar;
+
+  return pointTemplClone;
+};
+
 var fragment = document.createDocumentFragment();
-for (var j = 0; j < 1; j ++) {
-	console.log(pointsMap[j]);
-	fragment.appendChild(pointView(pointsMap[j]));
-}	
+for (var j = 0; j < 8; j++) {
+  fragment.appendChild(pointView(pointsMap[j]));
+}
 
 var mapElemet = document.querySelector('.map__pins');
 mapElemet.appendChild(fragment);
